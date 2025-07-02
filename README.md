e3a840b19362f01dd64ccd1a5c063e56f571d68d
Yeonhee-Ha <flfkrdl123@gmail.com>
2025-07-02 19:14:15 +0900

    feat: 여름학기 세션 1주차 과제

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # likelion_drf_hw
+
+drf 실습을 해보자
---
+++ singer/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Singer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 50)
+    content = models.TextField()
+    debut = models.DateField(auto_now=False)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
+class Song(models.Model):
+    id = models.AutoField(primary_key= True)
+    name = models.CharField(max_length=50)
+    singer = models.ForeignKey(Singer, blank = False, null = False, on_delete=models.CASCADE, related_name = "songs")
+    content = models.TextField()
+    release = models.DateField(auto_now = False)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
